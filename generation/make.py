2fa00176bde8c0d600d383751b71d6ff866ce77f
Anson <git@anson-cheung.com>
2021-07-20 23:30:36 +0800

    Fix stacking order, framerate

--- generation/make.py
+++ generation/make.py
@@ -16,12 +16,12 @@ def main():
             zip(leftarms, legs, panels, rightarms, spaces, stars, cockpits, windows)):
         # Star is base
         star.paste(space, mask=space)
+        star.paste(panel, mask=panel)
         star.paste(window, mask=window)
         star.paste(cockpit, mask=cockpit)
+        star.paste(leg, mask=leg)
         star.paste(leftarm, mask=leftarm)
         star.paste(rightarm, mask=rightarm)
-        star.paste(leg, mask=leg)
-        star.paste(panel, mask=panel)
 
         star.save(f'output/output{i:03}.png')
 
