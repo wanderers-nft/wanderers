261d3c359bd7f2f03f5ba4b579c81ed0e431a2df
Anson <git@anson-cheung.com>
2021-07-21 16:33:17 +0800

    Redo script structure

--- generation/getComponents.py
+++ generation/getComponents.py
@@ -1,75 +1,36 @@
+from typing import List
 from PIL import Image
 
 folder = "source"
 
 
-def get_cockpit():
-    cockpit = "cockpit/cockpit-1"
-    images = []
-    for i in range(1000, 1131):
-        f = Image.open(f'{folder}/{cockpit}/cockpit-{i}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_cockpit(manifest) -> List[Image]:
+    pass
 
 
-def get_leftarm():
-    leftarm = "leftarm/leftarm-1"
-    images = []
-    for i in range(1000, 1131):
-        f = Image.open(f'{folder}/{leftarm}/leftarm-{i}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_leftarm(manifest) -> List[Image]:
+    pass
 
 
-def get_rightarm():
-    leftarm = "rightarm/rightarm-1"
-    images = []
-    for i in range(1000, 1131):
-        f = Image.open(f'{folder}/{leftarm}/rightarm-{i}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_rightarm(manifest) -> List[Image]:
+    pass
 
 
-def get_legs():
-    legs = "legs"
-    images = []
-    for i in range(0, 131):
-        f = Image.open(f'{folder}/{legs}/legs{i:03}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_legs(manifest) -> List[Image]:
+    pass
 
 
-def get_panels():
-    panel = "panels/panel-d"
-    images = []
-    for i in range(0, 131):
-        f = Image.open(f'{folder}/{panel}/panel-d{i:03}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_panels(manifest) -> List[List[Image]]:
+    pass
 
 
-def get_space():
-    space = "space/planet-1"
-    images = []
-    for i in range(1000, 1131):
-        f = Image.open(f'{folder}/{space}/planet-{i}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_space(manifest) -> List[List[Image]]:
+    pass
 
 
-def get_stars():
-    stars = "stars"
-    images = []
-    for i in range(0, 131):
-        f = Image.open(f'{folder}/{stars}/stars{i:03}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_stars(manifest) -> List[Image]:
+    pass
 
 
-def get_windows():
-    windows = "windows/window-1"
-    images = []
-    for i in range(1000, 1131):
-        f = Image.open(f'{folder}/{windows}/window-{i}.png').convert('RGBA')
-        images.append(f)
-    return images
+def get_windows(manifest) -> List[Image]:
+    pass
--- generation/make.py
+++ generation/make.py
@@ -1,31 +1,59 @@
-from generation.getComponents import get_cockpit, get_stars, get_leftarm, get_legs, get_panels, get_rightarm, get_space, \
-    get_windows
+import json
+from dataclasses import dataclass
+
+from generation.getComponents import *
+
+number = 10
+
+
+@dataclass
+class Frames:
+    leftarm: List[Image]
+    legs: List[Image]
+    panels: List[List[Image]]
+    rightarm: List[Image]
+    space: List[List[Image]]
+    stars: List[Image]
+    cockpit: List[Image]
+    window: List[Image]
 
 
 def main():
-    leftarms = get_leftarm()
-    legs = get_legs()
-    panels = get_panels()
-    rightarms = get_rightarm()
-    spaces = get_space()
-    stars = get_stars()
-    cockpits = get_cockpit()
-    windows = get_windows()
-
-    for (i, (leftarm, leg, panel, rightarm, space, star, cockpit, window)) in enumerate(
-            zip(leftarms, legs, panels, rightarms, spaces, stars, cockpits, windows)):
-        # Star is base
-        star.paste(space, mask=space)
-        star.paste(panel, mask=panel)
-        star.paste(window, mask=window)
-        star.paste(cockpit, mask=cockpit)
-        star.paste(leg, mask=leg)
-        star.paste(leftarm, mask=leftarm)
-        star.paste(rightarm, mask=rightarm)
-
-        star.save(f'output/output{i:03}.png')
-
+    manifest = json.load(open('manifest_prototypev2.json'))
+    frames = get_attributes(manifest)
+    combine_attributes(frames)
     return
+
+
+def get_attributes(manifest) -> Frames:
+    # Get each of them according to manifest
+    leftarm = get_leftarm(manifest)
+    legs = get_legs(manifest)
+    panels = get_panels(manifest)
+    rightarms = get_rightarm(manifest)
+    spaces = get_space(manifest)
+    stars = get_stars(manifest)
+    cockpits = get_cockpit(manifest)
+    windows = get_windows(manifest)
+
+    return Frames(leftarm, legs, panels, rightarms, spaces, stars, cockpits, windows)
+
+
+def combine_attributes(frames: Frames):
+    # TODO
+    pass
+    # for (i, (leftarm, leg, panel, rightarm, space, star, cockpit, window)) in enumerate(
+    #         zip(leftarms, legs, panels, rightarms, spaces, stars, cockpits, windows)):
+    #     # Star is base
+    #     star.paste(space, mask=space)
+    #     star.paste(panel, mask=panel)
+    #     star.paste(window, mask=window)
+    #     star.paste(cockpit, mask=cockpit)
+    #     star.paste(leg, mask=leg)
+    #     star.paste(leftarm, mask=leftarm)
+    #     star.paste(rightarm, mask=rightarm)
+    # 
+    #     star.save(f'output/output{i:03}.png')
 
 
 if __name__ == "__main__":
