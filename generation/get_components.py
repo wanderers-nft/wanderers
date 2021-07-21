f4cf6c946b5aa2f2e3afcc64a28cd6100b97f3d7
Anson <git@anson-cheung.com>
2021-07-21 19:17:55 +0800

    Script randomisation

---
+++ generation/get_components.py
@@ -0,0 +1,79 @@
+import os.path
+import random
+from pprint import pprint
+from typing import List
+
+from PIL import Image
+
+folder = "source"
+
+
+def chance(prob):
+    return random.random() < prob
+
+
+# Get categories
+def fetch_categories(manifest) -> List:
+    # Get all categories
+    categories = manifest["categories"]
+
+    if manifest["multiple"]:
+        categories = list(filter(lambda cat: chance(cat["rarity"]), categories))
+    else:
+        categories = random.choices(
+            population=categories,
+            weights=[x["rarity"] for x in categories],
+            k=1
+        )
+
+    return categories
+
+
+# Get list of files to fetch
+def files_in_category(category) -> List:
+    files = category["files"]
+
+    if category["multiple"]:
+        files = list(filter(lambda cat: chance(cat["rarity"]), files))
+    else:
+        files = random.choices(
+            population=files,
+            weights=[x["rarity"] for x in files],
+            k=1
+        )
+
+    return files
+
+
+def get_files(attrib: str, category: str, files: str) -> List:
+    images = []
+    for i in range(0, 131):
+        file_name = f'{folder}/{attrib}/{category}/{files}/{files}_{i:05}.png'
+
+        # FIXME: All files should have 131 frames
+        if not os.path.isfile(file_name):
+            file_name = f'{folder}/{attrib}/{category}/{files}/{files}_{0:05}.png'
+
+        images.append(Image.open(file_name).convert('RGBA'))
+
+    return images
+
+
+def get(attrib) -> List:
+    categories = fetch_categories(attrib)
+    files_list = [(category["category"], files_in_category(category)) for category in categories]
+    pprint(files_list)
+    x = [
+        get_files(
+            attrib["attribute"],
+            c[0],
+            f["file"]
+        )
+        for c in files_list for f in c[1]
+    ]
+    return x
+    # Read files
+
+
+def get_stars() -> List:
+    return get_files("stars", "stars", "stars")
--- generation/make.py
+++ generation/make.py
@@ -1,47 +1,80 @@
 import json
 from dataclasses import dataclass
+from typing import List
 
-from generation.getComponents import *
+from tqdm import tqdm
+
+from generation.get_components import get, get_stars
 
 number = 10
 
 
 @dataclass
 class Frames:
-    leftarm: List[Image]
-    legs: List[Image]
-    panels: List[List[Image]]
-    rightarm: List[Image]
-    space: List[List[Image]]
-    stars: List[Image]
-    cockpit: List[Image]
-    window: List[Image]
+    leftarm: List
+    legs: List
+    panels: List[List]
+    rightarm: List
+    space: List[List]
+    stars: List
+    cockpit: List
+    window: List
+
+
+class Manifest:
+    def __init__(self, manifest):
+        self.manifest = manifest
+
+    def attribute(self, attr: str):
+        return [x for x in self.manifest if x["attribute"] == attr][0]
 
 
 def main():
-    manifest = json.load(open('manifest_prototypev2.json'))
+    manifest = Manifest(json.load(open('manifest_prototypev2.json')))
     frames = get_attributes(manifest)
     combine_attributes(frames)
     return
 
 
-def get_attributes(manifest) -> Frames:
+def get_attributes(manifest: Manifest) -> Frames:
     # Get each of them according to manifest
-    leftarm = get_leftarm(manifest)
-    legs = get_legs(manifest)
-    panels = get_panels(manifest)
-    rightarms = get_rightarm(manifest)
-    spaces = get_space(manifest)
-    stars = get_stars(manifest)
-    cockpits = get_cockpit(manifest)
-    windows = get_windows(manifest)
+    leftarm = get(manifest.attribute("leftarm"))
+    legs = get(manifest.attribute("legs"))
+    panels = get(manifest.attribute("panels"))
+    rightarm = get(manifest.attribute("rightarm"))
+    spaces = get(manifest.attribute("space"))
+    stars = get_stars()
+    cockpit = get(manifest.attribute("cockpit"))
+    window = get(manifest.attribute("window"))
 
-    return Frames(leftarm, legs, panels, rightarms, spaces, stars, cockpits, windows)
+    return Frames(leftarm, legs, panels, rightarm, spaces, stars, cockpit, window)
 
 
 def combine_attributes(frames: Frames):
-    # TODO
-    pass
+    for (n, star) in tqdm(enumerate(frames.stars)):
+        for space in [x[n] for x in frames.space]:
+            star.paste(space, mask=space)
+
+        for panel in [x[n] for x in frames.panels]:
+            star.paste(panel, mask=panel)
+
+        for window in [x[n] for x in frames.window]:
+            star.paste(window, mask=window)
+
+        for cockpit in [x[n] for x in frames.cockpit]:
+            star.paste(cockpit, mask=cockpit)
+
+        for leg in [x[n] for x in frames.legs]:
+            star.paste(leg, mask=leg)
+
+        for leftarm in [x[n] for x in frames.leftarm]:
+            star.paste(leftarm, mask=leftarm)
+
+        for rightarm in [x[n] for x in frames.rightarm]:
+            star.paste(rightarm, mask=rightarm)
+
+        star.save(f'output/output{n:05}.png')
+
     # for (i, (leftarm, leg, panel, rightarm, space, star, cockpit, window)) in enumerate(
     #         zip(leftarms, legs, panels, rightarms, spaces, stars, cockpits, windows)):
     #     # Star is base
