e4bcafbf0d81954cf861d242fbace05cd28b0ab7
Anson <git@anson-cheung.com>
2021-07-22 18:17:43 +0800

    Update mp4 script, update manifest_prototypev2.json

--- generation/ff.sh
+++ generation/ff.sh
@@ -1,1 +1,8 @@
-ffmpeg -r 15 -start_number 0 -i "output%05d.png" -c:v libx264 -pix_fmt yuv420p out.mp4
+cd outout || exit
+
+for i in {0..49}
+do
+  cd "$i" || exit
+  ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -c:v libx264 -pix_fmt yuv420p ../"${i}".mp4
+  cd ..
+  done
