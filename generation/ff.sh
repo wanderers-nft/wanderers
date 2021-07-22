05a62216779554eb3fce387ba88cd65ee3607a34
Anson <git@anson-cheung.com>
2021-07-22 19:43:40 +0800

    Fix quality

--- generation/ff.sh
+++ generation/ff.sh
@@ -1,8 +1,8 @@
-cd outout || exit
+cd output || exit
 
 for i in {0..49}
 do
   cd "$i" || exit
-  ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -c:v libx264 -pix_fmt yuv420p ../"${i}".mp4
+  ffmpeg -r 15 -start_number 0 -i "${i}_%05d.png" -c:v libx264 -crf 18 -pix_fmt yuv420p ../"${i}".mp4
   cd ..
   done
