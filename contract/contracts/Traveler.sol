03c70bcf57b91dfb0f92524cc4462cb4a38e8188
Anson <git@anson-cheung.com>
2021-07-22 21:23:27 +0800

    Add enable sale mechanic

--- contract/contracts/Traveler.sol
+++ contract/contracts/Traveler.sol
@@ -21,19 +21,31 @@ contract Traveler is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
     // Maximum mints per transaction
     uint256 public maxPerTx;
 
+    // Is sale on?
+    bool public sale;
+
     constructor(address fundsTo_, uint256 maxSupply_, uint256 maxPerTx_) ERC721("Traveler", "TRAVEL") {
         fundsTo = fundsTo_;
         maxSupply = maxSupply_;
         maxPerTx = maxPerTx_;
+        sale = false;
     }
 
     function updateFundsTo(address newFundsTo) public onlyOwner {
         fundsTo = newFundsTo;
     }
 
+    function enableSale() public onlyOwner {
+        sale = true;
+    }
+
     function safeMint(address to, uint256 quantity) public {
-        require(super.totalSupply() + quantity < maxSupply, "Total supply will exceed limit");
+        require(sale, "Sale disabled");
+
+        require(quantity != 0, "Requested quantity cannot be zero");
         require(quantity <= maxPerTx, "Requested quantity more than maximum");
+
+        require(super.totalSupply() + quantity < maxSupply, "Total supply will exceed limit");
 
         for (uint256 i = 0; i < quantity; i++) {
             _safeMint(to, _tokenIdCounter.current());
