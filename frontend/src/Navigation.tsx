f6768315d83827648c8afabfe819d8139201762f
Anson <git@anson-cheung.com>
2021-08-01 00:30:07 +0800

    Add sample navbar, web3 dependencies

---
+++ frontend/src/Navigation.tsx
@@ -0,0 +1,34 @@
+import "./Navigation.css";
+
+export function Navigation() {
+    return (
+        <div className="navbar">
+            <div>
+                <h1>WANDERERS</h1>
+            </div>
+            <div>
+                <p>Buy</p>
+            </div>
+            /
+            <div>
+                <p>Gallery</p>
+            </div>
+            /
+            <div>
+                <p>About</p>
+            </div>
+            /
+            <div>
+                <p>Roadmap</p>
+            </div>
+            /
+            <div>
+                <p>Team</p>
+            </div>
+            /
+            <div>
+                <p>Follow</p>
+            </div>
+        </div>
+    )
+}
