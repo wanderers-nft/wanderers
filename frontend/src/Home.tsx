e59e24ce3f7a734a28eaf54a90820649d1a9bdbe
Anson <git@anson-cheung.com>
2021-08-01 06:31:16 +0800

    Fix section links

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,18 +4,13 @@ import {Col, Container, Row} from "react-bootstrap";
 export function Home() {
     return (
         <Container fluid="xxl">
+            <section id="about"/>
             <About/>
+            <section id="roadmap"/>
             <Row>
                 <Col>
                     <h1>
                         Roadmap
-                    </h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h1>
-                        Buy now
                     </h1>
                 </Col>
             </Row>
--- frontend/src/Navigation.tsx
+++ frontend/src/Navigation.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import {HashLink as Link} from "react-router-hash-link";
 import {Col, Container, Row} from "react-bootstrap";
 
 export function Navigation() {
