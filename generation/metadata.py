9cde82e7ca4d3fd47397b280c1d7bf47fbaa242d
Anson <git@anson-cheung.com>
2021-08-05 06:51:27 +0800

    Update metadata.py to include CID

--- generation/metadata.py
+++ generation/metadata.py
@@ -1,25 +1,31 @@
 import json
 import os
 
+cid = "REPLACEME"
+
 
 def main():
     names = json.load(open("names.json"))
     for filename in os.scandir("output/metadata"):
-        transformed = transform_json(json.load(open(filename.path)), names)
+        transformed = transform_json(
+            json.load(open(filename.path)),
+            names,
+            filename.name.split('.')[0]
+        )
 
         with open(filename.path, "w") as o:
             json.dump(transformed, o)
 
 
-def transform_json(data, names):
-    transformed = []
+def transform_json(data, names, file_name):
+    metadata = {"animation_url": f"ipfs://{cid}/{file_name}.mp4", "attributes": []}
     for x in data.items():
         # For each sub-item in attribute
         for y in x[1]:
-            transformed.append(
+            metadata["attributes"].append(
                 {"trait_type": names[x[0]], "value": names[y]}
             )
-    return transformed
+    return metadata
 
 
 if __name__ == "__main__":
