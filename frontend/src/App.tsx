e5bdbfabf7aec160e86e267996eb02e5c39c0b63
Anson <git@anson-cheung.com>
2021-10-02 00:04:54 +0800

    Update cf beacon token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
                 </Switch>
             </HashRouter>
-            <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "eb38fdd04d394e8f9d021125831dd0fe"}'/>
+            <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c1bd613e33e34ef0b3b01451a0ba3d43"}'></script>
         </div>
     );
 }
