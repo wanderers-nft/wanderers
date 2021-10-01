cab8a631384dcdc5575ce80b859584f0f8279ab0
Anson <git@anson-cheung.com>
2021-10-02 00:15:34 +0800

    Move cf beacon to index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,6 @@ function App() {
 
                 </Switch>
             </HashRouter>
-            <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c1bd613e33e34ef0b3b01451a0ba3d43"}'></script>
         </div>
     );
 }
